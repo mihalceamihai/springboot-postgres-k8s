dc6b4417e342946f975364bdb124f5fed8165b60
mihalceamihai <mihalcea.mihaialexandru@gmail.com>
2021-05-14 01:01:36 +0300

    modify table metrics structure

--- src/main/java/com/example/postgres/springbootpostgresdocker/model/Metrics.java
+++ src/main/java/com/example/postgres/springbootpostgresdocker/model/Metrics.java
@@ -3,6 +3,7 @@ package com.example.postgres.springbootpostgresdocker.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,8 +21,10 @@ public class Metrics extends MetricsAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    private String location;
     private String collector;
     private String metricType;
+    private boolean isSyncronized;
     private float value;
 
 }
