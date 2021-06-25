5a51887a2ba35ec159d5cc5e875e7f1329290d99
Mihai-Alexandru Mihalcea <mihalcea.mihaialexandru@gmail.com>
2021-06-25 18:47:04 +0300

    Update entrypoint for aarch64

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#exec /usr/bin/java ${JVM_OPTS} -jar "${@}"
-exec /usr/local/openjdk-8/bin/java ${JVM_OPTS} -jar "${@}"
+exec /usr/bin/java ${JVM_OPTS} -jar "${@}"
+#exec /usr/local/openjdk-8/bin/java ${JVM_OPTS} -jar "${@}"
